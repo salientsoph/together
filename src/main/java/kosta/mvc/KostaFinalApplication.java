3af4cd53a8b6f33a5ac0d41104c7e814a3d1a20e
salientsoph <69945903+salientsoph@users.noreply.github.com>
2021-11-24 16:32:14 +0900

    Initial commit

---
+++ src/main/java/kosta/mvc/KostaFinalApplication.java
@@ -0,0 +1,13 @@
+package kosta.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KostaFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KostaFinalApplication.class, args);
+	}
+
+}
