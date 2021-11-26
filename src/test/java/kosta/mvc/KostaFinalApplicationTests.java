cbb0402901b0c13018876b5acd7e0a9a75cec53b
milkinhale <62606734+milkinhale@users.noreply.github.com>
2021-11-26 21:00:01 +0900

    사업자

--- src/test/java/kosta/mvc/KostaFinalApplicationTests.java
+++ src/test/java/kosta/mvc/KostaFinalApplicationTests.java
@@ -9,8 +9,10 @@ import org.springframework.test.annotation.Commit;
 
 import kosta.mvc.domain.Customer;
 import kosta.mvc.domain.Region;
+import kosta.mvc.domain.Seller;
 import kosta.mvc.repository.CustomerRepository;
 import kosta.mvc.repository.RegionRepository;
+import kosta.mvc.repository.SellerRepository;
 
 @SpringBootTest
 @Transactional
@@ -22,6 +24,9 @@ class KostaFinalApplicationTests {
 	
 	@Autowired
 	private RegionRepository regionRep;
+	
+	@Autowired
+	private SellerRepository sellerRep;
 
 	@Test
 	void contextLoads() {
@@ -45,5 +50,52 @@ class KostaFinalApplicationTests {
 		regionRep.save(Region.builder().regionCode(5).regionName("대구/경북").build());
 		regionRep.save(Region.builder().regionCode(6).regionName("광주/전라").build());
 		regionRep.save(Region.builder().regionCode(7).regionName("강원/제주").build());
+	}
+	
+	@Test
+	public void sellerInsert() {
+		sellerRep.save(Seller.builder().sellerId("seller01")
+									 .sellerPwd("1234")
+									 .sellerName("셀러1")
+									 .sellerNickname("셀러닉1")
+									 .sellerEmail("seller@sell.com")
+									 .sellerPhone("010-111-111")
+									 .sellerRegisterNumber(12345678)
+									 .build());
+		sellerRep.save(Seller.builder().sellerId("seller02")
+				 .sellerPwd("1234")
+				 .sellerName("셀러2")
+				 .sellerNickname("셀러닉2")
+				 .sellerEmail("seller@sell.com")
+				 .sellerPhone("010-111-111")
+				 .sellerRegisterNumber(12345678)
+				 .build());
+
+		sellerRep.save(Seller.builder().sellerId("seller03")
+									 .sellerPwd("1234")
+									 .sellerName("셀러3")
+									 .sellerNickname("셀러닉3")
+									 .sellerEmail("seller@sell.com")
+									 .sellerPhone("010-111-111")
+									 .sellerRegisterNumber(12345678)
+									 .build());
+
+		sellerRep.save(Seller.builder().sellerId("seller04")
+									 .sellerPwd("1234")
+									 .sellerName("셀러4")
+									 .sellerNickname("셀러닉4")
+									 .sellerEmail("seller@sell.com")
+									 .sellerPhone("010-111-111")
+									 .sellerRegisterNumber(12345678)
+									 .build());
+
+		sellerRep.save(Seller.builder().sellerId("seller05")
+									 .sellerPwd("1234")
+									 .sellerName("셀러5")
+									 .sellerNickname("셀러닉5")
+									 .sellerEmail("seller@sell.com")
+									 .sellerPhone("010-111-111")
+									 .sellerRegisterNumber(12345678)
+									 .build());
 	}
 }
